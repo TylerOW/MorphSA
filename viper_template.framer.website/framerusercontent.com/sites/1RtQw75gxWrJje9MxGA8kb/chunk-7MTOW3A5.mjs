4d6730b742e5ce1d13ee2c705778bb6c248107a8
TylerOW <123558433+TylerOW@users.noreply.github.com>
2025-07-07 04:03:30 +0800

    Push

---
+++ viper_template.framer.website/framerusercontent.com/sites/1RtQw75gxWrJje9MxGA8kb/chunk-7MTOW3A5.mjs
@@ -0,0 +1,2 @@
+function e(a,t){return{description:"Viper, a creative portfolio framer template, is a perfect fit for personal portfolios, designers, freelancers, agencies, and photographers.",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/ilOcLpm39pNtcDUvOCamC8eoI.png",title:"Viper - Portfolio & Agency"}}export{e as a};
+//# sourceMappingURL=chunk-7MTOW3A5.mjs.map
